c6390203b390f9a940f0b0a0781d39bee543fed7
Sanchezedg14 <sanchezedgar_3@hotmail.com>
2022-10-08 22:03:24 -0500

    Se modifica package.json y puerto

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(3050, () => console.log('Server on'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Server on'));
 
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,'./views/home.html'))
